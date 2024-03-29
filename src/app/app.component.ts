5361946f9df8583d4f51c2bf5fd261a544d11bdd
Rena Migdali <rena.mygdali@hotmail.com>
2024-03-29 18:34:00 +0200

    new component person-table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   name = 'Rena';
-
-  person = {
-    givenName: 'Rena',
-    surName: 'Migdali',
-    age: 36,
-    email: 'renamygdali@aueb.gr'
-  }
 }
