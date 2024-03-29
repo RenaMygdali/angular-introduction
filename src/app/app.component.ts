bed6b6685f9d6674ca1b447e14cfb2da7384f414
Rena Migdali <rena.mygdali@hotmail.com>
2024-03-29 21:00:33 +0200

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,21 @@ import { Component } from '@angular/core';
 import { PersonTableComponent } from './components/person-table/person-table.component';
 import { Person } from './shared/interfaces/person';
 import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PersonTableComponent, EventBindExampleComponent],
+  imports: [
+    RouterLink, 
+    RouterOutlet, 
+    PersonTableComponent, 
+    EventBindExampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  name = 'Rena';
+  
 
   person0 = {
     givenName: 'Rena',
