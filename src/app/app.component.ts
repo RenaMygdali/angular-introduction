6fb2b4f56176a95509c2edd779c6d2ead4f7e2e7
Rena Migdali <rena.mygdali@hotmail.com>
2024-04-01 20:05:21 +0300

    fancy app menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,16 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 import { Person } from './shared/interfaces/person';
 import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { ListGroupMenuComponent } from './components/list-group-menu/list-group-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterLink, 
-    RouterOutlet],
+    RouterOutlet,
+    ListGroupMenuComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
